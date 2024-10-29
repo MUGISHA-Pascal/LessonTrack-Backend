30603bc0c6128c62fe7369c89d5a503ec3aa99d1
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-29 13:45:37 +0200

    adding the feedback  model

---
+++ src/models/Feedback.ts
@@ -0,0 +1,49 @@
+import postgresConnectionSequelize from "../config/postgres";
+
+const { DataTypes } = require("sequelize");
+
+const Feedback = postgresConnectionSequelize.define(
+  "Feedback",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "NO ACTION",
+      onDelete: "CASCADE",
+    },
+    course_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "courses",
+        key: "id",
+      },
+      onUpdate: "NO ACTION",
+      onDelete: "SET NULL",
+    },
+    feedback_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "feedback",
+    schema: "public",
+    timestamps: false,
+  }
+);
+
+export default Feedback;
