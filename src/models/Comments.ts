8821d1a97a7da5afb0f7df374f9733d7839fd324
MUGISHA-Pascal <mugishapascal2008@gmail.com>
2024-10-29 13:53:58 +0200

    adding the comments model

---
+++ src/models/Comments.ts
@@ -0,0 +1,53 @@
+import postgresConnectionSequelize from "../config/postgres";
+
+const { DataTypes } = require("sequelize");
+
+const Comment = postgresConnectionSequelize.define(
+  "Comment",
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "users",
+        key: "id",
+      },
+      onUpdate: "NO ACTION",
+      onDelete: "CASCADE",
+    },
+    course_id: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "courses",
+        key: "id",
+      },
+      onUpdate: "NO ACTION",
+      onDelete: "CASCADE",
+    },
+    comment_text: {
+      type: DataTypes.TEXT,
+      allowNull: false,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+    updated_at: {
+      type: DataTypes.DATE,
+      defaultValue: DataTypes.NOW,
+    },
+  },
+  {
+    tableName: "comments",
+    schema: "public",
+    timestamps: false,
+  }
+);
+
+export default Comment;
